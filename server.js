d5babdf7639aea791fc0ec40cc4ade8551ba6516
Young Won2 <youngw417@MacBook.local>
2020-01-20 12:04:10 -0800

    mvp

--- server.js
+++ server.js
@@ -3,7 +3,19 @@ const express = require('express');
 const db = require('./data/dbConfig.js');
 
 const server = express();
+const accountRouter = require('./account/accountRouter');
 
 server.use(express.json());
+server.use('/api/accounts', accountRouter);
 
+server.get('/', (req, res) => {
+  res.status(200).send('Welcome to Database');
+});
+
+server.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({
+    errorMessage: 'Something is wrong'
+  });
+});
 module.exports = server;
